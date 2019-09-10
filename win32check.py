402d041e8b75de8ccceff62dfcce14ce07ac1683
Keenan Kunzelman <keekunz@gmail.com>
2019-09-10 19:55:08 -0400

    added drive class which has capability to store metadata about drives with ntfs on it.

--- win32check.py
+++ win32check.py
@@ -6,7 +6,6 @@ import os
 libc = ctypes.CDLL(ctypes.util.find_library('c'), use_errno=True)
 libc.mount.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_ulong, ctypes.c_char_p)
 
-
 #Run the command sudo blkid and then recieve its output as a bytes
 #Decode using utf-8 and then split on \n
 def grab_drives():
@@ -28,11 +27,6 @@ def locate_winfs(drives):
     return win_drives
 
 
-
-
-
-
-
 # use and unpack drive object here
 def mount_drive(source, target, fs, options=''):
   ret = libc.mount(source, target, fs, 0, options)
@@ -42,36 +36,21 @@ def mount_drive(source, target, fs, options=''):
     format(source, fs, target, options, os.strerror(errno)))
 
 
-
-
-
-
-
-
-
-
-
-
 def find_payload():
     pass
 
 
 class Drive:
-    # make getters and setters you stupid incel
-    # so you can have partially populated obj at run time
     def __init__(self):
         self.source = ""
         self.target = "/mnt"
         self.fs = ""
-        self.options = ""
+        self.options = "rw"
 
     def set_source(self, source):
         self.source = source
-
     def set_fs(self, fs):
         self.fs = fs
-    def set_options(self, options):
-        self.options = options
 
     def get_source(self):
         return self.source
@@ -81,33 +60,32 @@ class Drive:
         return self.fs
     def get_options(self):
         return self.options
-
-def main():
-    drives = grab_drives()
-    raw_win_drives = locate_winfs(drives)
-    print(len(raw_win_drives))
-
+    
+def storewin_drives(raw_win_drives):
     win_drives = []
-    #THIS SHIT IS ASS BACKWARDS AND YOURA A FUCKING INCEL
-    # IF YOU DONT REFACTIOR IT IN THE MORINING
-
     for i in range(len(raw_win_drives)):
         temp_drive = Drive()
         temp_raw_drive = raw_win_drives[i].split()
         for attribute in temp_raw_drive:
-            if "/dev" in attribute:
-                #gotta trim the last character here
-                temp_drive.set_source = attribute
+            if "/dev" in attribute:         
+                attribute = list(attribute)
+                attribute.pop()
+                attribute = "".join(attribute)
+                temp_drive.set_source(attribute)     
             elif "TYPE" in attribute:
-                temp_drive.set_fs = attribute
-
-        temp_drive.set_source = raw_win_drives[i]
-        temp_drive.set_target = raw_win_drives[i]
-        temp_drive.set_fs = raw_win_drives[i]
-        temp_drive.set_options = raw_win_drives[i]
-
+                temp_drive.set_fs(attribute)
         win_drives.append(temp_drive)
+    return win_drives
 
+
+def main():
+    drives = grab_drives()
+    raw_win_drives = locate_winfs(drives)
+    win_drives = storewin_drives(raw_win_drives)
+    for drive in win_drives:
+        print(drive.get_source())
+        print(drive.get_fs())
+  
 
 
 
